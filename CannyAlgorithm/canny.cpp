dc6cef67b796ce3230edc352a06abdeccb0eab90
Lorenzo Pergamo <72994442+MrPericle@users.noreply.github.com>
2023-07-13 16:52:20 +0200

    Update canny.cpp

--- CannyAlgorithm/canny.cpp
+++ CannyAlgorithm/canny.cpp
@@ -3,111 +3,82 @@
 #include <stdlib.h>
 #include <iostream>
 
+using std::cout;
+using std::endl;
 using namespace cv;
-using namespace std;
 
-Mat src, dest, edges;
-int ht = 100, lt = 30;
+int lth = 80;
+int hth = 120;
 
-// Function to perform Gaussian blur on the source image
-void applyGaussianBlur(const Mat& src, Mat& dest)
-{
-    Size kernelS(3, 3);
-    GaussianBlur(src, dest, kernelS, 0);
+void caclculateMagAndAng(const Mat& src, Mat& mag, Mat& ang){
+    Mat dx,dy;
+    Sobel(src,dx,CV_32F,1,0);
+    Sobel(src,dy,CV_32F,0,1);
+    //mag = abs(dx) + abs(dy);
+    magnitude(dx,dy,mag);
+    // dx.convertTo(dx,CV_32F);
+    // dy.convertTo(dy,CV_32F);
+
+    phase(dx,dy,ang,true);
+
 }
 
-// Function to calculate gradient magnitude and angle
-void calculateGradient(const Mat& src, Mat& sobelX, Mat& sobelY, Mat& mag, Mat& ang)
-{
-    Sobel(src, sobelX, src.type(), 1, 0);
-    Sobel(src, sobelY, src.type(), 0, 1);
-    mag = abs(sobelX) + abs(sobelY);
-    sobelX.convertTo(sobelX, CV_32FC1);
-    sobelY.convertTo(sobelY, CV_32FC1);
-    phase(sobelX, sobelY, ang, true);
-}
-
-// Function to perform non-maximum suppression on the gradient magnitude image
-void performNonMaxSuppression(Mat& mag, Mat& ang)
-{
-    copyMakeBorder(mag, mag, 1, 1, 1, 1, BORDER_CONSTANT, Scalar(0));
-
-    for (int i = 1; i < mag.rows - 1; ++i)
-    {
-        for (int j = 1; j < mag.cols - 1; j++)
-        {
-            float ang_val = ang.at<float>(i, j) > 180 ? ang.at<float>(i, j) - 360 : ang.at<float>(i, j);
-
-            if ((ang_val <= -157.5 && ang_val > 157.5) || (ang_val > -22.5 && ang_val <= 22.5))
-            {
-                if (mag.at<uchar>(i, j) < mag.at<uchar>(i, j - 1) || mag.at<uchar>(i, j) < mag.at<uchar>(i, j + 1))
-                    mag.at<uchar>(i, j) = 0;
+void NMS(const Mat& mag,const Mat& ang, Mat& nmsOut){
+    copyMakeBorder(mag,nmsOut,1,1,1,1,BORDER_CONSTANT,Scalar(0));
+    for(int y = 1; y < mag.rows; y++){
+        for(int x = 1; x < mag.cols; x++){
+            float angVal = ang.at<float>(y-1,x-1) > 180 ? ang.at<float>(y,x)-180 : ang.at<float>(y,x);
+            if(angVal >=0 && angVal <= 22.5 || angVal <= 180 && angVal > 157.5 ){
+                if(nmsOut.at<float>(y,x) < nmsOut.at<float>(y,x - 1) || nmsOut.at<float>(y,x) < nmsOut.at<float>(y,x + 1) ){
+                    nmsOut.at<float>(y,x) = 0;
+                }
             }
-            else if ((ang_val <= -112.5 && ang_val > -157.5) || (ang_val > 22.5 && ang_val <= 67.5))
-            {
-                if (mag.at<uchar>(i, j) < mag.at<uchar>(i + 1, j + 1) || mag.at<uchar>(i, j) < mag.at<uchar>(i - 1, j - 1))
-                    mag.at<uchar>(i, j) = 0;
-            }
-            else if ((ang_val <= -67.5 && ang_val > -112.5) || (ang_val > 67.5 && ang_val <= 112.5))
-            {
-                if (mag.at<uchar>(i, j) < mag.at<uchar>(i + 1, j) || mag.at<uchar>(i, j) < mag.at<uchar>(i - 1, j))
-                    mag.at<uchar>(i, j) = 0;
-            }
-            else if ((ang_val <= -22.5 && ang_val > -67.5) || (ang_val > 112.5 && ang_val <= 157.5))
-            {
-                if (mag.at<uchar>(i, j) < mag.at<uchar>(i + 1, j - 1) || mag.at<uchar>(i, j) < mag.at<uchar>(i - 1, j + 1))
-                    mag.at<uchar>(i, j) = 0;
-            }
+            else if(angVal > 22.5 && angVal <= 67.5){
+                if(nmsOut.at<float>(y,x) < nmsOut.at<float>(y+1,x+1) || nmsOut.at<float>(y,x) < nmsOut.at<float>(y-1,x-1) ){
+                    nmsOut.at<float>(y,x) = 0;
+                }
+            } 
+            else if(angVal > 67.5 && angVal <= 112.5){
+                if(nmsOut.at<float>(y,x) < nmsOut.at<float>(y-1,x) || nmsOut.at<float>(y,x) < nmsOut.at<float>(y+1,x) ){
+                    nmsOut.at<float>(y,x) = 0;
+                }
+            } 
+            else if(angVal > 112.5 && angVal <= 157.5 ){
+                if(nmsOut.at<float>(y,x) < nmsOut.at<float>(y+1,x-1) || nmsOut.at<float>(y,x) < nmsOut.at<float>(y-1,x+1) ){
+                    nmsOut.at<float>(y,x) = 0;
+                }
+            }  
         }
     }
 }
 
-// Function to perform hysteresis thresholding on the gradient magnitude image
-void performHysteresisThresholding(Mat& mag, Mat& dest, int low_th, int high_th)
-{
-    dest = Mat::zeros(mag.rows - 1, mag.cols - 1, CV_8UC1);
-
-    for (int i = 1; i < mag.rows - 1; ++i)
-    {
-        for (int j = 1; j < mag.cols - 1; j++)
-        {
-            if (mag.at<uchar>(i, j) > high_th)
-                dest.at<uchar>(i, j) = 255;
-            else if (mag.at<uchar>(i, j) < low_th)
-                dest.at<uchar>(i, j) = 0;
-            else if (mag.at<uchar>(i, j) <= high_th && mag.at<uchar>(i, j) >= low_th)
-            {
-                bool strong_n = false;
-
-                for (int x = -1; x <= 1 && !strong_n; x++)
-                {
-                    for (int y = -1; y <= 1 && !strong_n; y++)
-                    {
-                        if (mag.at<uchar>(i + x, j + y) > high_th)
-                            strong_n = true;
+void HThreshold(Mat& nmsOut, Mat& dest){
+    dest = Mat::zeros(nmsOut.rows-2,nmsOut.cols-2,CV_8U);
+    for(int y = 1; y < nmsOut.rows-1; y++){
+        for(int x = 1; x < nmsOut.cols;x++){
+            if(nmsOut.at<float>(y,x) > hth) dest.at<uchar>(y-1,x-1) = 255;
+            else if(nmsOut.at<float>(y,x)< lth) dest.at<uchar>(y-1,x-1) = 0;
+            else{
+                bool strongN = false;
+                for(int j = -1; j >= 1; j++){
+                    for(int i = -1; i >= 1; i++){
+                        if(nmsOut.at<float>(y + j,x + i) > hth) strongN = true;
                     }
                 }
-
-                if (strong_n)
-                    dest.at<uchar>(i - 1, j - 1) = 255;
+                if(strongN) dest.at<uchar>(y-1,x-1) = 255;
+                else dest.at<uchar>(y-1,x-1) = 0;
             }
         }
     }
 }
 
-// Callback function for the Canny threshold trackbars
-void CannyThreshold(int, void*)
-{
-    applyGaussianBlur(src, dest);
-    Mat sobelX, sobelY, mag, ang;
-    calculateGradient(dest, sobelX, sobelY, mag, ang);
-    performNonMaxSuppression(mag, ang);
-    performHysteresisThresholding(mag, dest, lt, ht);
-    cv::Canny(src, edges, lt, ht, 3, false);
-
-    imshow("cvCanny", edges);
-    imshow("Canny", dest);
-}
+void myCanny(const Mat& src, Mat& dest){
+    Mat blur,mag,ang,nmsOut;
+    GaussianBlur(src,blur,Size(5,5),0,0);
+    caclculateMagAndAng(src,mag,ang);
+    NMS(mag,ang,nmsOut);
+    HThreshold(nmsOut,dest);
+}   
 
 int main(int argc, char const* argv[])
 {
@@ -117,15 +88,16 @@ int main(int argc, char const* argv[])
         exit(0);
     }
 
-    src = imread(argv[1], IMREAD_GRAYSCALE);
+    Mat src = imread(argv[1], IMREAD_GRAYSCALE);
+    Mat dest;
+    // if (src.cols > 500 || src.rows > 500) {
+    //     cv::resize(src, src, cv::Size(0, 0), 0.5, 0.5); // resize for speed
+    // }
 
     imshow("src", src);
-    namedWindow("Canny");
-    cv::createTrackbar("Trackbar th", "src", &ht, 255, CannyThreshold);
-    cv::createTrackbar("Trackbar lh", "src", &lt, 255, CannyThreshold);
-    CannyThreshold(0, 0);
-
-    waitKey(0);
+    myCanny(src,dest);
+    imshow("dest",dest);
+    waitKey();
 
     return 0;
 }
